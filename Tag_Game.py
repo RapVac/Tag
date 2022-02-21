7675be0e22ba727a8ed2993bdd64122f141b2f4b
RapVac <89621646+RapVac@users.noreply.github.com>
2022-02-20 17:34:00 -0700

    More things

--- Tag_Game.py
+++ Tag_Game.py
@@ -11,6 +11,8 @@ canvas.pack()
 tk.update()
 t1=time.time()
 
+
+
 class Human:
     def __init__(self, canvas, color, speed):
         self.canvas=canvas
@@ -25,11 +27,14 @@ class Human:
         self.canvas.bind_all('<KeyPress-Down>', self.Move)
         self.canvas.bind_all('<KeyPress-Left>', self.Move)
         self.canvas.bind_all('<KeyPress-Right>', self.Move)
+        self.canvas.bind_all('<KeyPress-Shift_R>', self.Move)
+        self.s = 0
+        self.s_ind = False
 
     def Move(self, event):
         if event.keysym == 'Up':
             self.y = -self.speed
-            self.x=0
+            self.x = 0
         if event.keysym == 'Down':
             self.y = self.speed
             self.x=0
@@ -39,6 +44,26 @@ class Human:
         if event.keysym == 'Right':
             self.x = self.speed
             self.y=0
+        if event.keysym == 'Shift_R':
+            self.s_ind = True
+            if self.y != 0:
+                self.y = self.y*2
+            if self.x != 0:
+                self.x=self.x*2
+
+##########
+    def fast(self):
+        if self.s_ind == True:
+            self.s += 1
+            if self.s == 100*0.3:
+                self.s_ind = False
+                if self.y != 0:
+                    self.y = self.y / 2
+                    self.s = 0
+                if self.x != 0:
+                    self.x = self.x / 2
+                    self.s = 0
+               
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
         pos = self.canvas.coords(self.id)
@@ -63,8 +88,7 @@ class Hunter:
         self.y=0
         self.canvas_height = canvas.winfo_height()
         self.canvas_width = canvas.winfo_width()
-        self.your_it = False
-        self.p=1
+        self.game_over = False
         self.pts=0
 
     def draw(self):
@@ -86,21 +110,34 @@ class Hunter:
 
             
         if abs(h_x1-s_x1) < 10 and abs(h_y1-s_y1) < 10:
-            self.your_it = True
+            self.game_over = True
             time.sleep(1)
             t2 = time.time()
             ft="You lasted %s seconds" % round(t2-t1)
-            canvas.create_text(250, 250, text="Tag! Your it!", fill='dark blue', font=('Times', 30))
-            canvas.create_text(250, 285, text=ft, fill='dark blue', font=('Times', 22))
-            
+            canvas.create_text(250, 250, text="Tag!", fill='dark blue', font=('Times', 30))
+            canvas.create_text(250, 285, text=ft, fill='dark blue', font=('Times', 18))
+##            canvas.create_text(250, 285+28, text="Press 'r' to restart.", fill='dark blue', font=('Times', 18))
+
+##def restart(event):
+##    hunter.game_over = False
+    
             
 human = Human(canvas, 'blue', 3)
 hunter = Hunter(human, canvas, 'black', 2.2)
 
+
+
+start = 0
+##canvas.bind_all('<KeyPress-r>', restart)
+
 while 1:
-    if hunter.your_it == False:
+    if hunter.game_over == False:
         human.draw()
+        human.fast()
         hunter.draw()
     tk.update_idletasks()
     tk.update()
+    if start == 0:
+        time.sleep(1.5)
+        start += 1
     time.sleep(0.01)
